d94c70443abed00b3869d6f250fe690005296f09
s-wakasa <wakasa.s1129@gmail.com>
2016-03-08 18:35:55 +0900

    取得したXMLから画像が正しく表示されなかった問題を修正

--- app/item.php
+++ app/item.php
@@ -3,10 +3,52 @@
 require_once("../util/defineUtil.php");
 require_once("../util/scriptUtil.php");
 
-
-
-$url = "http://shopping.yahooapis.jp/ShoppingWebService/V1/itemLookup?appid=$appid&itemcode=$itemcode";
-$xml = simplexml_load_file($url);
-if ($xml["totalResultsReturned"] != 0) {//検索件数が0件でない場合,変数$hitsに検索結果を格納
-	$hits = $xml->Result->Hit;
+if(!empty($_GET['code'])){//配列チェックとエラー表示
+	$item_code = $_GET['code'];
+}else{
+	echo "不正なURLです<br/>";
+	die(return_top());
 }
+
+$url = "http://shopping.yahooapis.jp/ShoppingWebService/V1/itemLookup?appid=$appid&itemcode=$item_code&image_size=300";
+$xml = simplexml_load_file($url);
+if ($xml["totalResultsReturned"] != 0) {
+	$hits = $xml->Result->Hit;
+}else{
+	 echo "その商品ページは存在しません<br/>";
+	 die(return_top());
+}//APIが検索結果を返さなかった場合にエラー表示
+
+$name = h($hits->Name);
+$price = h($hits->Price);
+$image = h($hits->Image->Small);
+
+?>
+
+<html>
+    <head>
+        <meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
+        <title>かごいっぱいのゆめ　- 商品詳細</title>
+    </head>
+	<body>
+	<h1><a href="<?php echo ROOT_URL.TOP_URI ?>">かごいっぱいのゆめ - 仮想商品購入サイト </a></h1>
+
+	<font size="6"><?php echo $name."<br/>"; ?></font>
+
+	<font size="6" color="#ff0000">価格:<?php echo $price; ?>円</font><br>
+
+	<p><img src="<?php echo h($hits->ExImage->Url); ?>" /><br></p>
+
+	<p><?php echo h($hits->Headline); ?></p>
+
+	<p><?php echo h($hits->Review->Rate); ?><br></p>
+	
+	<form action="<?php echo ADD ?>" method="POST">
+	<input type="hidden" name="name"  value="<?php echo $name ?>">
+	<input type="hidden" name="price"  value="<?php echo $price ?>">
+	<input type="hidden" name="image"  value="<?php echo $image ?>"> <!-- 配列をPOSTするの無理っぽい -->
+	<input type="submit" name="btnSubmit" value="カートへ追加">
+	
+	</form>
+	</body>
+</html>
