f37d3e2b09db85d345be2c29cd00403811fce515
s-wakasa <wakasa.s1129@gmail.com>
2016-03-08 18:33:53 +0900

    新規

--- app/add.php
+++ app/add.php
@@ -1,1 +1,23 @@
 <?php
+//カートに追加完了ページ
+
+if(!empty($_COOKIE['access_count'])){
+	$count = $_COOKIE['access_count'];//2回目以降のアクセスはカウントに反映
+}else{
+	setcookie('access_count',"1");//notice回避の為に初期値1
+	$count = 0;
+}
+
+setcookie("name[$count]",$_POST['name']);
+setcookie("price[$count]",$_POST['price']);
+setcookie("image[$count]",$_POST['image']);
+
+if(!empty($_COOKIE["name"])){ $name = $_COOKIE["name"];}//ここでnoticeでた
+if(!empty($_COOKIE["price"])){ $price = $_COOKIE["price"];}
+if(!empty($_COOKIE["image"])){ $image = $_COOKIE["image"];}
+
+$count++;
+setcookie('access_count',$count);//アクセスカウント増加
+
+echo $_COOKIE['access_count'].'回アクセス'."<br/>";
+echo $name[0].$price[0].$image[0].'格納完了';
